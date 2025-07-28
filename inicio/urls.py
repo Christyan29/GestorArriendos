f1befab39189dbee833a06a91c2a4e532bcc2428
Cristian Macas <cvmacas@gmail.com>
2025-07-28 10:04:02 -0500

    se creo las plantillas para recuperar la contraseña

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from . import views
 from inicio.views import lista_arrendatarios
+from django.urls import include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+
     path('', views.bienvenida, name='bienvenida'),
     path('login/', views.vista_login, name='login'),
+    path('accounts/login/', views.vista_login, name='login'),
     path('nosotros/', views.vista_nosotros, name='nosotros'),
     path('contacto/', views.vista_contacto, name='contacto'),
     path('registrarse/', views.vista_registrarse, name='registrarse'),
@@ -14,6 +18,11 @@ urlpatterns = [
     path('contratos/', views.contratos_admin, name='contratos_admin'),
     path('reportes/', views.reportes_admin, name='reportes_admin'),
     path('arrendatarios/', lista_arrendatarios, name='lista_arrendatarios'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 
