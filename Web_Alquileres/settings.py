d79cbad68d641a938f727c04476493e8cebea8ff
Cristian Macas <cvmacas@gmail.com>
2025-08-04 01:31:58 -0500

    se actualizo la pagina de recupar la contraseña de la pagina login

--- Web_Alquileres/settings.py
+++ Web_Alquileres/settings.py
@@ -30,15 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'inicio',
-]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -60,6 +52,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- inicio/urls.py
+++ inicio/urls.py
@@ -46,11 +46,6 @@ urlpatterns = [
     path('contratos/', views.contratos_admin, name='contratos_admin'),
     path('reportes/', views.reportes_admin, name='reportes_admin'),
     path('arrendatarios/', lista_arrendatarios, name='lista_arrendatarios'),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 
 ]
 
